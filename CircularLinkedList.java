fa338f5e40109589eaebcb90d0994bb49c610487
Yatharth <52836348+yatharth-16@users.noreply.github.com>
2020-08-07 21:57:37 +0530

    Add files via upload

---
+++ CircularLinkedList.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class CircularLinkedList {
+
+	
+	public static Node<Integer> head=null;
+    public static Node<Integer> tail=null;
+	
+    
+    public static Node<Integer> InsertAtBegining()
+    {
+    	Scanner s = new Scanner(System.in);
+		int data = s.nextInt();
+		Node<Integer> last=null;
+		while(data!=-1)
+		{
+			Node<Integer> newNode = new Node<Integer>(data);
+			
+			if(head==null)
+			{
+				head= newNode;
+				tail = newNode;
+				last=newNode;
+				
+			}
+			else
+			{   
+				
+				head= newNode; 
+				head.next=tail;
+				tail=head;
+			} 
+			
+			data = s.nextInt();
+		}
+		last.next=head;
+		return head;
+    }
+    
+    
+	public static Node<Integer> InsertAtEnd()
+	{
+	
+		Scanner s = new Scanner(System.in);
+		int data = s.nextInt();
+		while(data!=-1)
+		{
+			Node<Integer> newNode = new Node<Integer>(data);
+			
+			if(head==null)
+			{
+				head= newNode;
+				tail = newNode;
+			}
+			else
+			{
+				tail.next= newNode; 
+				tail= newNode;
+				tail.next=head;
+			} 
+			
+			data = s.nextInt();
+		}
+		return head;
+	}
+	
+	
+	public static void  print(Node<Integer> head)
+	{
+		if(head==null)
+		{
+			return;
+		}
+		
+		Node<Integer> tail = head;
+		
+		do
+		{
+			System.out.println(tail.data);
+			tail=tail.next;
+			
+		}while(tail!=head);
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		//Node<Integer> head = InsertAtEnd();
+		
+		Node<Integer> head = InsertAtBegining();
+		
+		print(head);
+		
+		
+		
+		
+
+	}
+
+}
